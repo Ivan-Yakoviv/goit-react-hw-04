2e0e6b7d8e8b9e6f1099eaed6a1d81b5adddeafb
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-07-30 02:08:57 +0300

    Update article-api.js

--- src/article-api.js
+++ src/article-api.js
@@ -1,9 +1,19 @@
 import axios from "axios";
 
- const API_KEY = "otTGWoIxQNzliFudXJVId93wApk-ehHVJdDDWbun6I4";
-axios.defaults.baseURL = "https://hn.algolia.com/api/v1";
+const API_KEY = "otTGWoIxQNzliFudXJVId93wApk-ehHVJdDDWbun6I4";
+ axios.defaults.baseURL = "https://api.unsplash.com";
+  axios.defaults.headers.common["Authorization"] = `Client-ID ${API_KEY}`;
 
-export const fetchArticlesWithTopic = async topic => {
-  const response = await axios.get(`/search?query=${topic}`);
-  return response.data.hits;
+
+export const fetchArticlesWithTopic = async (query,page) => {
+   const response = await axios.get("/search/photos", {
+    params: {
+      query,
+      per_page: 16,
+      page,
+      orientation: "landscape",
+    },
+  });
+
+  return response.data;
 };
