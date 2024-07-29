008c65f4c9dc08fb5459e34d780801e4dc172341
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-07-30 01:48:32 +0300

    Update article-api.js

--- src/article-api.js
+++ src/article-api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+ const API_KEY = "otTGWoIxQNzliFudXJVId93wApk-ehHVJdDDWbun6I4";
 axios.defaults.baseURL = "https://hn.algolia.com/api/v1";
 
 export const fetchArticlesWithTopic = async topic => {
