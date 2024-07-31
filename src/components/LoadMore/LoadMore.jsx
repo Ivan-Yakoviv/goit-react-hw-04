34142c36b4ccf1b04d8a53152e857168bf24a3cf
Ivan-Yakoviv <ivasykvv@gmail.com>
2024-07-31 22:33:36 +0300

    add scroll to top

--- src/components/LoadMore/LoadMore.jsx
+++ src/components/LoadMore/LoadMore.jsx
@@ -1,9 +1,19 @@
 import s from './LoadMore.module.css'
+import { animateScroll as scroll } from 'react-scroll';
 
-const LoadMore = ({onMore}) => {
+const LoadMore = ({ onMore }) => {
+    
+     const handleScrollToTop = () => {
+    scroll.scrollToTop({
+      duration: 500,
+      smooth: 'easeInOutQuart',
+    });
+  };
+
   return (
     <div>
-      <button onClick={onMore}>Load more</button>
+          <button onClick={onMore} className={s.moreBtn}>Load more</button>
+          <button onClick={handleScrollToTop}>Scroll to Top</button>
     </div>
   )
 }
